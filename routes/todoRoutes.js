aadb4d19b84b158488f03661f6b1c222cc047e90
umashankar <umashankarp33@gmail.com>
2025-01-22 10:08:05 +0530

    create login and register form routes

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { deleteTodo, getTodo, getTodos, postTodo, updateTodo } from '../controllers/todoControllers.js';
+import { auth } from '../middlewares/auth.js';
 
 let router=express.Router()
 
-router.post("/",postTodo)
-router.get("/",getTodos)
-router.get("/:id",getTodo)
-router.put("/:id",updateTodo)
-router.delete("/:id",deleteTodo)
+router.post("/",auth,postTodo)
+router.get("/",auth,getTodos)
+router.get("/:id",auth,getTodo)
+router.put("/:id",auth,updateTodo)
+router.delete("/:id",auth,deleteTodo)
 
 
 // router.route("/").get(getTodos).post(postTodo);
