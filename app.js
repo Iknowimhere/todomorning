8af87a1371413bd4a9089481d57b9c5d01054f5a
umashankar <umashankarp33@gmail.com>
2025-01-23 09:34:11 +0530

    add middleware and global error handler

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import db from './config/db.js';
 import todoRouter from './routes/todoRoutes.js'
 import userRouter from './routes/userRoutes.js'
 import methodOverride from 'method-override'
+import cookieParser from 'cookie-parser';
 let app = express();
 //db connection
 db();
 //registering template engine
 app.set("view engine","ejs")
 
+app.use(cookieParser())
 //processes incoming json data
 // app.use(express.json());
 //processes incoming form data
@@ -21,6 +23,12 @@ app.use(methodOverride('_method'))
 
 app.use("/api/v1/todo",todoRouter);
 app.use("/api/v1/user",userRouter);
+
+//global error handling middleware
+app.use((err,req,res,next)=>{
+    // console.log(err.stack);
+    return res.status(400).json(err.message);
+})
 
 
 export default app;
